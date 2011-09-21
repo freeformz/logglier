e3062a2e3562958e0f7344126925f8b554902913
Dan Peterson <dpiddy@gmail.com>
2011-09-21 10:45:19 -0300

    Expand retrying for HTTP.

--- lib/logglier/client/http/sync.rb
+++ lib/logglier/client/http/sync.rb
@@ -7,18 +7,30 @@ module Logglier
       # Used to wrap and setup Net::HTTP as we need it
       class NetHTTPProxy
 
+        RETRY_EXCEPTIONS = [
+          TimeoutError, OpenSSL::SSL::SSLError, Errno::EPIPE,
+          EOFError, Errno::ECONNRESET, Errno::ETIMEDOUT,
+          Errno::ECONNREFUSED
+        ]
+
+        RETRIES = 3
+
+        attr_accessor :failsafe
+
         # @param [URI] input_uri URI to deliver messages to
         # @param [Hash] opts Option hash
         # @option [Integer] read_timeout Read timeout for the http session. defaults to 2
         # @option [Integer] open_timeout Open timeout for the http session. defaults to 2
         # @option [Integer] verify_mode OpenSSL::SSL::VERIFY_* constant
         # @option [String] ca_file Path to the ca file
+        # @option [IO] failsafe Where to `#puts` delivery errors. defaults to `$stderr`
         def initialize(input_uri, opts={})
           @input_uri = input_uri
           @verify_mode = opts[:verify_mode] || OpenSSL::SSL::VERIFY_PEER
           @ca_file = opts[:ca_file]
           @read_timeout = opts[:read_timeout] || 5
           @open_timeout = opts[:open_timeout] || 5
+          @failsafe = opts[:failsafe] || $stderr
           @format = opts[:format] ? opts[:format].to_sym : nil
           @headers = {}
           if @format == :json
@@ -32,21 +44,22 @@ module Logglier
         #
         # @param [String] message The message to deliver
         def deliver(message)
-          retried = false
+          retries = 0
+
           begin
             @http.request_post(@input_uri.path, message, @headers)
-          # We're using persistent connections, so connection can be closed by the other side
-          # after a timeout. Don't consider it an error, just retry once.
-          rescue Errno::ECONNRESET
-            unless retried
-              retried = true
+          rescue *RETRY_EXCEPTIONS => e
+            if retries < RETRIES
+              retries += 1
+              failsafe_retrying(e, message, retries)
+              sleep retries
               connect!
               retry
             else
-              $stderr.puts "WARNING: connection was reset while posting message: #{message}"
+              failsafe_errored(e, message)
             end
-          rescue TimeoutError, OpenSSL::SSL::SSLError, EOFError, Errno::ECONNRESET => e
-            $stderr.puts "WARNING: #{e.class} posting message: #{message}"
+          rescue Exception => e
+            failsafe_errored(e, message)
           end
         end
 
@@ -68,6 +81,14 @@ module Logglier
           @http.read_timeout = @read_timeout
           @http.open_timeout = @open_timeout
 
+        end
+
+        def failsafe_retrying(exception, message, retries)
+          @failsafe.puts "WARNING: [#{retries}/#{RETRIES}] caught #{exception} while attempting to deliver: #{message}"
+        end
+
+        def failsafe_errored(exception, message)
+          @failsafe.puts "ERROR: caught #{exception} while attempting to deliver: #{message}"
         end
       end
 
--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -7,7 +7,9 @@ describe 'HTTP' do
     @http.stub!(:read_timeout=)
     @http.stub!(:open_timeout=)
     Net::HTTP.stub!(:new).and_return(@http)
-    @proxy = Logglier::Client::HTTP::NetHTTPProxy.new(URI.parse('http://localhost:9292'))
+    @failsafe = StringIO.new
+    @proxy = Logglier::Client::HTTP::NetHTTPProxy.new(URI.parse('http://localhost:9292'), :failsafe => @failsafe)
+    @proxy.stub(:sleep)
   end
 
   it "retries after connection is reset" do
@@ -16,14 +18,69 @@ describe 'HTTP' do
     @proxy.deliver('message')
   end
 
-  it "retries only once connection is reset" do
-    @http.should_receive(:request_post).exactly(2).times.and_raise Errno::ECONNRESET
-    @proxy.deliver('message')
+  it "defaults its failsafe to $stderr" do
+    proxy = Logglier::Client::HTTP::NetHTTPProxy.new(URI.parse('http://localhost:9292'))
+    proxy.failsafe.should == $stderr
   end
 
-  it "fails for other errors" do
-    @http.should_receive(:request_post).once.and_raise EOFError
-    @proxy.deliver('message')
+  describe "error handling" do
+    Logglier::Client::HTTP::NetHTTPProxy::RETRY_EXCEPTIONS.each do |error|
+      context "for #{error}" do
+        context "with total failure" do
+          before do
+            @http.should_receive(:request_post).exactly(4).times.and_raise error
+          end
+
+          it "retries 3 times then fails" do
+            @proxy.deliver('message')
+          end
+
+          it "logs messages to the failsafe along the way" do
+            @proxy.deliver('message')
+
+            @failsafe.rewind
+            lines = @failsafe.readlines
+            lines.size.should == 4
+            lines[0..2].each {|l| l.should =~ /^WARNING/ }
+            lines.last.should =~ /^ERROR/
+          end
+        end
+
+        context "if things start working again" do
+          before do
+            @http.should_receive(:request_post).exactly(3).times.and_raise error
+            @http.should_receive(:request_post)
+          end
+
+          it "is successful if things start working again" do
+            @proxy.deliver('message')
+          end
+
+          it "logs messages to the failsafe along the way" do
+            @proxy.deliver('message')
+
+            @failsafe.rewind
+            lines = @failsafe.readlines
+            lines.size.should == 3
+            lines[0..2].each {|l| l.should =~ /^WARNING/ }
+          end
+        end
+      end
+    end
+
+    context "for an unknown error" do
+      it "logs a message" do
+        error = Class.new(StandardError)
+
+        @http.should_receive(:request_post).and_raise error
+        @proxy.deliver('message')
+
+        @failsafe.rewind
+        lines = @failsafe.readlines
+        lines.size.should == 1
+        lines.first.should =~ /^ERROR/
+      end
+    end
   end
 
   describe "json format" do
