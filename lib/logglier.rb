100ccfd9066006ef5be6e5a5512662a6418be480
Kapil Kale <kapil.kale.07@gmail.com>
2013-03-09 14:55:31 -0800

    fix text logging issues with simpleformatter

--- lib/logglier.rb
+++ lib/logglier.rb
@@ -12,8 +12,22 @@ module Logglier
     client = Logglier::Client.new(url, opts)
     logger = ActiveSupport::Logger.new(client)
 
-    if client.respond_to?(:formatter)
-      logger.formatter = client.formatter
+    # KK: Logglier's formatter is great for JSON but nasty for text.
+    # It spits out lines like this:
+    #
+    # 2013-03-09 14:21:23 -0800 severity=DEBUG,
+    #
+    # ...which are useless. Moreover, the logs show up without newlines in
+    # the console, which makes them illegible.
+    # Let's just use SimpleFormatter (the default) when we're dealing with text logs.
+    # This is a hack and the real fix should be fixing the formatter within Logglier.
+
+    if client.respond_to?(:formatter) &&
+      if opts[:format] == :json
+        logger.formatter = client.formatter
+      else
+        logger.formatter = ActiveSupport::Logger::SimpleFormatter.new
+      end
     elsif client.respond_to?(:datetime_format)
       logger.datetime_format = client.datetime_format
     end
